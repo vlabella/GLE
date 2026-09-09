1bf14f3ef2d4f981921a91862a74d6481b71ef88
Vincent LaBella <vlabella@albany.edu>
2026-09-09 15:45:15 -0400

    removed uneeded and outdated hash_map in favor of STD C++ map

--- src/gle/tokens/StringKeyHash.h
+++ src/gle/tokens/StringKeyHash.h
@@ -38,34 +38,6 @@
 
 #ifndef STRINGKEYHASH_H
 #define STRINGKEYHASH_H
-
-// #ifndef _MSC_VER
-// 	// Use hash table except when compiling with MSVC
-// 	//#define GLE_USE_HASHTABLE
-// #else
-// 	#pragma warning( disable : 4996 )
-// #endif
-
-// #define EXT_HASH
-// #ifdef _MSC_VER
-// 	#undef EXT_HASH
-// #endif
-// #ifdef GCC2
-// 	#undef EXT_HASH
-// #endif
-// #ifdef EXT_HASH
-// //	#include <ext/hash_map>
-// #else
-// 	#ifdef _MSC_VER
-// 	#include <hash_map>
-// 	using namespace stdext;
-// //	typedef hash _Hash;
-// 	#else
-// 	#include <hash_map.h>
-// 	#endif
-// #endif
-
-
 #include <algorithm>
 #include <string>
 #include <string.h>
@@ -73,16 +45,6 @@
 #include <utility>
 #include <iostream>
 #include <vector>
-
-// using namespace std;  should not reside in header file
-
-// #ifndef GCC2
-// #ifndef _MSC_VER
-// 	// gcc on macOS is complaining about this; not sure if it's needed
-// 	// using namespace __gnu_cxx;  // using gnu extensions such as "hash"
-// #endif
-// #endif
-
 #include "RefCount.h"
 #include "BinIO.h"
 
@@ -107,48 +69,6 @@ public:
 	}
 };
 
-// #ifdef GLE_USE_HASHTABLE
-
-// struct eq_name_hash_key {
-// 	inline bool operator() (const name_hash_key& s1, const name_hash_key& s2) const {
-// 	    return s1 == s2;
-// 	}
-// };
-
-// struct hash_name_hash_key {
-// 	inline size_t operator() (const name_hash_key& s) const {
-// 		#ifdef _MSC_VER
-// 		return stdext::hash_value<const char *>(s.c_str());
-// 		#else
-// 		return hash<const char *>()(s.c_str());
-// 		#endif
-// 	}
-// };
-
-// template <class ElemType> class StringKeyIterator : public std::hash_map<name_hash_key, ElemType>::iterator {
-// };
-
-// struct eq_int_key {
-// 	inline bool operator() (int s1, int s2) const {
-// 	    return s1 == s2;
-// 	}
-// };
-
-// struct hash_int_key {
-// 	inline size_t operator() (int s) const {
-// 		#ifdef _MSC_VER
-// 		return stdext::hash_value<int>(s);
-// 		#else
-// 		return std::hash<int>()(s);
-// 		#endif
-// 	}
-// };
-
-// template <class ElemType> class IntKeyIterator : public std::hash_map<int, ElemType>::iterator {
-// };
-
-// #else
-
 struct lt_name_hash_key {
 	bool operator()(const name_hash_key& s1, const name_hash_key& s2) const {
 		return s1 < s2;
@@ -161,18 +81,12 @@ struct lt_int_key {
 	}
 };
 
-//#endif
-
 /**************************************************************************************************
  * Hashtable string -> ElemType                                                                   *
  **************************************************************************************************/
 
 template <class ElemType> class StringBasicHash :
-#ifdef GLE_USE_HASHTABLE
-	public std::hash_map<name_hash_key, ElemType, hash_name_hash_key, eq_name_hash_key> {
-#else
 	public std::map<name_hash_key, ElemType, lt_name_hash_key> {
-#endif
 };
 
 /**************************************************************************************************
@@ -180,18 +94,12 @@ template <class ElemType> class StringBasicHash :
  **************************************************************************************************/
 
 template <class ElemType> class IntBasicHash :
-#ifdef GLE_USE_HASHTABLE
-	public std::hash_map<int, ElemType, hash_int_key, eq_int_key> {
-#else
 	public std::map<int, ElemType, lt_int_key> {
-#endif
 };
 
 /**************************************************************************************************
  * Hashtable string -> int
  **************************************************************************************************/
-
-//#ifdef GLE_USE_HASHTABLE
 
 class StringIntHash : public StringBasicHash<int> {
 public:
@@ -199,13 +107,9 @@ public:
  	void add_item(const name_hash_key& key, int elem);
 };
 
-//#endif
-
 /**************************************************************************************************
  * Hashtable string -> void*                                                                      *
  **************************************************************************************************/
-
-// //#ifdef GLE_USE_HASHTABLE
 
 class StringVoidPtrHash : public StringBasicHash<void*> {
 public:
@@ -213,8 +117,6 @@ public:
  	void add_item(const name_hash_key& key, void* elem);
  	void deleteRecursive(int depth);
 };
-
-// //#endif
 
 /**************************************************************************************************
  * Hashtable int -> int                                                                           *
@@ -241,15 +143,7 @@ public:
  **************************************************************************************************/
 
 template <class ElemType> class StringKeyHash :
-#ifdef GLE_USE_HASHTABLE
-    #ifdef _MSC_VER
-	public std::hash_map<name_hash_key, ElemType , stdext::hash_compare<name_hash_key,eq_name_hash_key> > {
-    #else
-	public std::hash_map<name_hash_key, ElemType , hash_name_hash_key, eq_name_hash_key> {
-    #endif
-#else
 	public std::map<name_hash_key, ElemType, lt_name_hash_key> {
-#endif
 public:
 	ElemType try_add(const name_hash_key& key) {
 		typename StringKeyHash<ElemType>::iterator i = this->find(key);
@@ -296,11 +190,7 @@ public:
  **************************************************************************************************/
 
 template <class ElemType> class IntKeyHash :
-#ifdef GLE_USE_HASHTABLE
-	public std::hash_map<int, ElemType, hash_int_key, eq_int_key> {
-#else
 	public std::map<int, ElemType, lt_int_key> {
-#endif
 public:
 	ElemType try_get(int key) {
 		typename IntKeyHash<ElemType>::const_iterator i = this->find(key);
